25cbd325ce136f05cc922c0796d95969fd4439dd
lee <lsh3384@gmail.com>
2022-11-01 22:50:17 +0900

    applied column merge and row merge

--- thirdTable.js
+++ thirdTable.js
@@ -13,23 +13,34 @@ const {
   
     
 const border_right_double = {
-//   top: {
-//     style: BorderStyle.DOUBLE,
-//     size: 1,
-//   },
-//   bottom: {
-//     style: BorderStyle.DOUBLE,
-//     size: 1,
-//   },
-//   left: {
-//     style: BorderStyle.DOUBLE,
-//     size: 1,
-//   },
   right: {
     style: BorderStyle.DOUBLE,
     size: 10,
   },
 };
+
+const border_right_bottom_double = {
+  right: {
+    style: BorderStyle.DOUBLE,
+    size: 10,
+  },
+  bottom: {
+    style: BorderStyle.DOUBLE,
+    size: 15,
+  },
+};
+
+const border_right_top_double = {
+  right: {
+    style: BorderStyle.DOUBLE,
+    size: 10,
+  },
+  top: {
+    style: BorderStyle.DOUBLE,
+    size: 15,
+  },
+};
+
 
 const border_left_double = {
   left: {
@@ -38,12 +49,26 @@ const border_left_double = {
   },
 };
 
+const border_top_double = {
+  top: {
+    style: BorderStyle.DOUBLE,
+    size: 15,
+  },
+};
+
+const border_bottom_double = {
+  bottom: {
+    style: BorderStyle.DOUBLE,
+    size: 15,
+  },
+};
+
   const firstRow = new TableRow({
     children: [
       new TableCell({
         width: {
           size: 1500,
-          type: WidthType,
+          type: WidthType.DXA,
         },
         borders: border_right_double,
         children: [
@@ -56,8 +81,8 @@ const border_left_double = {
       }),
       new TableCell({
         width: {
-          size: 3000,
-          type: WidthType,
+          size: 2800,
+          type: WidthType.DXA,
         },
         borders: border_left_double,
         children: [
@@ -70,8 +95,8 @@ const border_left_double = {
       }),
       new TableCell({
         width: {
-          size: 2500,
-          type: WidthType,
+          size: 2200,
+          type: WidthType.DXA,
         },
         children: [
           new Paragraph({
@@ -84,7 +109,7 @@ const border_left_double = {
       new TableCell({
         width: {
           size: 3000,
-          type: WidthType,
+          type: WidthType.DXA,
         },
         children: [
           new Paragraph({
@@ -100,9 +125,270 @@ const border_left_double = {
       rule: HeightRule.ATLEAST,
     },
   });
+
+
+  const secondRow = new TableRow({
+    children: [
+      new TableCell({
+        width: {
+          size: 1500,
+          type: WidthType.DXA,
+        },
+        borders: border_right_double,
+        children: [
+          new Paragraph({
+            text: "명            칭",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2800+2200+3000,
+          type: WidthType.DXA,
+        },
+        borders: border_left_double,
+        columnSpan: 3,
+        children: [
+          new Paragraph({
+            text: "전기 자동차의 충전 제어 장치",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+    ],
+    height: {
+      value: 300,
+      rule: HeightRule.ATLEAST,
+    },
+  });
   
+
+  const thirdRow = new TableRow({
+    children: [
+      new TableCell({
+        width: {
+          size: 1500,
+          type: WidthType.DXA,
+        },
+        borders: border_right_double,
+        children: [
+          new Paragraph({
+            text: "우선권 번호",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2800,
+          type: WidthType.DXA,
+        },
+        borders: border_left_double,
+        children: [
+          new Paragraph({
+            text: "해당없음",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2200,
+          type: WidthType.DXA,
+        },
+        children: [
+          new Paragraph({
+            text: "우선권 주장일",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 3000,
+          type: WidthType.DXA,
+        },
+        children: [
+          new Paragraph({
+            text: "해당없음",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+    ],
+    height: {
+      value: 300,
+      rule: HeightRule.ATLEAST,
+    },
+  });
+
+
+  const fourthRow = new TableRow({
+    children: [
+      new TableCell({
+        width: {
+          size: 1500,
+          type: WidthType.DXA,
+        },
+        borders: border_right_bottom_double,
+        children: [
+          new Paragraph({
+            text: "IPC 분류",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2800,
+          type: WidthType.DXA,
+        },
+        borders: border_bottom_double,
+        children: [
+          new Paragraph({
+            text: "B60L-058/10",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2200,
+          type: WidthType.DXA,
+        },
+        borders: border_bottom_double,
+        children: [
+          new Paragraph({
+            text: "조사기간",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 3000,
+          type: WidthType.DXA,
+        },
+        borders: border_bottom_double,
+        children: [
+          new Paragraph({
+            text: "2022.02.15 ~ 2022.04.11",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+    ],
+    height: {
+      value: 300,
+      rule: HeightRule.ATLEAST,
+    },
+  });
+
+  const fifthRow = new TableRow({
+    children: [
+      new TableCell({
+        width: {
+          size: 1500,
+          type: WidthType.DXA,
+        },
+        borders: border_right_top_double,
+        rowSpan: 2,
+        children: [
+          new Paragraph({
+            text: "서치분류",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 1400,
+          type: WidthType.DXA,
+        },
+        borders: border_top_double,
+        children: [
+          new Paragraph({
+            text: "CPC",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2200,
+          type: WidthType.DXA,
+        },
+        borders: border_top_double,
+        columnSpan: 2,
+        children: [
+          new Paragraph({
+            text: "B50L-058-10",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+    ],
+    height: {
+      value: 300,
+      rule: HeightRule.ATLEAST,
+    },
+  });
+
+  const sixthRow = new TableRow({
+    children: [
+      new TableCell({
+        width: {
+          size: 1400,
+          type: WidthType.DXA,
+        },
+        borders: border_left_double,
+        children: [
+          new Paragraph({
+            text: "FI",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+      new TableCell({
+        width: {
+          size: 2200,
+          type: WidthType.DXA,
+        },
+        columnSpan: 2,
+        children: [
+          new Paragraph({
+            text: "",
+            alignment: AlignmentType.CENTER,
+          }),
+        ],
+        verticalAlign: VerticalAlign.CENTER,
+      }),
+    ],
+    height: {
+      value: 300,
+      rule: HeightRule.ATLEAST,
+    },
+  });
+
+
+
   const firstTable = new Table({
-    rows: [firstRow],
+    rows: [firstRow, secondRow, thirdRow, fourthRow, fifthRow, sixthRow],
   });
   
   module.exports = firstTable;
